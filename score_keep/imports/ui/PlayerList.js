4c83e953ff535cf9b5a0d5738b275a0b9d18be95
WilliamJPalmer <deity@mac.com>
2017-11-28 12:46:47 -0600

    added notes the the PlayerList.js

--- score_keep/imports/ui/PlayerList.js
+++ score_keep/imports/ui/PlayerList.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Player from './Player'
+/*can remove the import Player from the client/main.js because
+it is imported here and client/main.js imports PlayerList
+Since Player.js and PlayerList.js are in the same folder, the
+location path does not include ../imports/ui/
+*/
 
 export default class PlayerList extends React.Component {
   renderPlayers(){
