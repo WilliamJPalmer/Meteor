9705c67696d971f27b262dbcfa3a557a2ca99f3c
WilliamJPalmer <deity@mac.com>
2018-01-07 21:01:04 -0600

    removed code for routes and authenticated users because of new users.js and routes.js files

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,62 +1,15 @@
 import 'babel-polyfill';
 import {Meteor} from 'meteor/meteor';
-import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
 import {Tracker} from 'meteor/tracker';
+import { routes, onAuthChange } from '../imports/routes/routes'
 
-import Signup from '../imports/ui/Signup';
-import Link from '../imports/ui/Link';
-import NotFound from '../imports/ui/NotFound';
-import Login from '../imports/ui/Login';
 
-const unauthenticatedPages = ['/', '/signup'];// pages that don't need to be logged in to see
-const authenticatedPages = ['/links'];//avaialble only is logged in
-const onEnterPublicPage = () =>{
-  if (Meteor.userId()){
-    //browserHistory.push('/links');
-    /* .push adds the pathname to the browserHistory. This means that if the browser's
-    back button is clicked, the page will go to the page that pushed to the /links page.
-    Using .replace rather than .push will overwrite the current page name with the
-    redirected page name.
-    the .push path from page to page(if user is logged in) and using browser back button
-    blankpage -> google.com -> localhost:3000 -.push-> localhost:3000/links <-back- localhost:3000 -.push-> localhost:3000/links
-
-    the .replace path from page to page(if user is logged in) and using browser back button
-    bankpage -> google.com -> localhost:3000 -.replace-> localhost:3000/links <-back- google.com*/
-    browserHistory.replace('/links');//.replace instead of .push
-  }
-};
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()){
-    //console.log("onEnterPrivatePage method");
-    browserHistory.replace('/');//.replace instead of .push
-    //console.log(browserHistory.getCurrentLocation().pathname);
-  }
-};
-const routes = (
-  <Router history={browserHistory}>
-    <Route path='/' component={Login} onEnter={onEnterPublicPage}/>
-    <Route path='/signup' component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path='/links' component={Link} onEnter={onEnterPrivatePage}/>
-    <Route path='*' component={NotFound}/>
-  </Router>
-);
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId(); //takes the truthy or falsey and turns it into a true boolena value.
   // a single ! with flip the value. two !! will return true or false.
-  const pathname = browserHistory.getCurrentLocation().pathname;//gets the current page.
-  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);//checks to see if the current page is in the unauthenticatedPages array
-  const isAuthenticatedPage = authenticatedPages.includes(pathname);//checks to see if the current page is in the authenticatedPages array
-
-  if (isAuthenticated && isUnauthenticatedPage){
-    browserHistory.replace('/links')// if user is logged in and navigates to an unauthenticate page, goes to links page
-    //.replace instead of .push
-  }else if (!isAuthenticated && isAuthenticatedPage){
-    browserHistory.replace('/')// if user is NOT logged in and navigates to an authenticate page, goes to login page
-    //.replace instead of .push
-  };
+  onAuthChange(isAuthenticated);
 });
 
 Meteor.startup(() => {
--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,55 +1,12 @@
 import 'babel-polyfill';
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
-import { Accounts } from 'meteor/accounts-base';
+
+import '../imports/api/users';
+/*Calls the imports/api/user.js file and runs it. There are no exports returned
+so no need for the from portion*/
+
 
 Meteor.startup(() => {
   // code to run on server at startup
 
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
-    try{
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({email:email})//ES6 shorthand lets this be written as .validate({ email })
-    } catch(err){
-      throw new Meteor.Error(400, err.message)//err.message is the message that was thrown in the generic
-      // and that can be attached to the Meteor.Error
-    }
-
-    return true;
-  });
-/*
-  try {
-    //throw new Error ('new error message');//generic error message
-    throw new Meteor.Error (400, 'Must be a valid email');//always takes two arguments
-    //first is a code based error that could be a string or actual error code
-    //second is the reason for invalid.
-  } catch (e){
-    console.log(e);
-  }*/
-  // const employeeSchema = new SimpleSchema ({
-  //   name: {
-  //     type: String,
-  //     min: 1,
-  //     max: 200,
-  //   },
-  //   hourlyWage: {
-  //     type: Number,
-  //     min: 1,
-  //   },
-  //   email: {
-  //     type: String,
-  //     regEx: SimpleSchema.RegEx.Email
-  //   }
-  // });
-  //
-  // employeeSchema.validate({
-  //   name: 'Marla',
-  //   hourlyWage:45,
-  //   email: 'me@mac.com'
-  // })
 });
