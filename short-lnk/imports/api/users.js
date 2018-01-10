3e95cef74e3bbe07fd4b1f91240c7c5a4a7056e2
WilliamJPalmer <deity@mac.com>
2018-01-10 15:54:43 -0600

    removed the try and catch method now that startup.simple-schema-configuration.js has been added to customize error messages

--- short-lnk/imports/api/users.js
+++ short-lnk/imports/api/users.js
@@ -4,17 +4,14 @@ import { Accounts } from 'meteor/accounts-base';
 
 Accounts.validateNewUser((user) => {
   const email = user.emails[0].address;
-  try{
-    new SimpleSchema({
-      email: {
-        type: String,
-        regEx: SimpleSchema.RegEx.Email
-      }
-    }).validate({email:email})//ES6 shorthand lets this be written as .validate({ email })
-  } catch(err){
-    throw new Meteor.Error(400, err.message)//err.message is the message that was thrown in the generic
-    // and that can be attached to the Meteor.Error
-  }
+
+  new SimpleSchema({
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    }
+  }).validate({email:email});//ES6 shorthand lets this be written as .validate({ email })
+
 
   return true;
 });
