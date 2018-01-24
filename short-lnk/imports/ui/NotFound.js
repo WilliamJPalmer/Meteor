0bf268a32b2f7c864711734153e5eff09611874a
WilliamJPalmer <deity@mac.com>
2018-01-23 22:43:05 -0600

    converted the code into Statelesss Functional Component.

--- short-lnk/imports/ui/NotFound.js
+++ short-lnk/imports/ui/NotFound.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-  render() {
-    return <p>Page not found component</p>;
-  }
-}
+// export default class NotFound extends React.Component {
+//   render() {
+//     return <p>Page not found component</p>;
+//   }
+// }
+//     SFC version 1
+// const NotFound = () => {
+//   return (
+//     <p>Page not found component</p>
+//   );
+// }
+//
+// export default NotFound;
+
+//   SFC version 2
+ export default () => {
+   return <p>NotFound component Shitlicker!!!!!</p>
+ }
